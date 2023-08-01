67706077ad8d00c5c506d8c8f8b1e4d215aef609
himikat123 <himikat123@gmail.com>
2023-08-01 23:28:08 +0200

    added automatic release compression

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,14 @@ const inline = require('gulp-inline');
 const uglify = require('gulp-uglify');
 const minifyCss = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
+const zip = require('gulp-zip');
+const clean = require('gulp-clean');
 
 gulp.task('inline', function() {
-    return gulp.src('build/index.html')
+    return gulp.src('./build/index.html')
         .pipe(inline({
-            base: 'build/',
+            base: './build/',
             js: function() {
                 return uglify({
                     mangle: false
@@ -15,10 +18,32 @@ gulp.task('inline', function() {
             },
             css: [minifyCss({level: {1: {specialComments: 0}}}), autoprefixer],
         }))
-        .pipe(gulp.dest('release')
+        .pipe(gulp.dest('./release')
     );
 });
 
+gulp.task('rename', function() {
+    return gulp.src("./release/index.html", { base: process.cwd() })
+        .pipe(rename({
+            dirname: "",
+            basename: "City_ID_Finder",
+            extname: ".html"
+        }))
+        .pipe(gulp.dest("./release"));
+});
+
+gulp.task('zip', function() {
+	return gulp.src('./release/City_ID_Finder.html')
+		.pipe(zip('City_ID_Finder.zip'))
+		.pipe(gulp.dest('./release'))
+    }
+);
+
+gulp.task('delete', function() {
+    return gulp.src('./release/*.html', {read: false})
+        .pipe(clean())
+});
+
 gulp.task('default', gulp.series(
-    'inline'
+    'inline', 'rename', 'zip', 'delete'
 ));
